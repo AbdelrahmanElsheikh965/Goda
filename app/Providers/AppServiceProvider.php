40ac8b6da1876a8e68830eb7ae4611a688f1a047
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2025-01-09 00:43:58 +0200

    feat: removing the db migration blocking line

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        SharedData::getInstance()->HandleAllSharedData();
+        # You must comment this line just to migrate for the first time.
+        // SharedData::getInstance()->HandleAllSharedData();
     }
 }
